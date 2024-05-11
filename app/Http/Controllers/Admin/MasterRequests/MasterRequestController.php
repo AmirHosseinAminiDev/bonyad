f687117da0f4c09c5115eca2f0cf8ecbd7c0c62d
AmirHosseinAmini <amirhosseinaminidev@gmail.com>
2024-05-11 19:53:43 +0330

    created model for this branch but requires documents finish yet

---
+++ app/Http/Controllers/Admin/MasterRequests/MasterRequestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin\MasterRequests;
+
+use App\Http\Controllers\Controller;
+use App\Models\MasterRequest;
+use Illuminate\Http\Request;
+
+class MasterRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MasterRequest $masterRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MasterRequest $masterRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MasterRequest $masterRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MasterRequest $masterRequest)
+    {
+        //
+    }
+}
