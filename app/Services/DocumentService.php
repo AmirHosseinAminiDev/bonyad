fdc7f7424f8e56937212bb042a8b0e9aa51b0d5c
AmirHosseinAmini <amirhosseinaminidev@gmail.com>
2024-05-12 08:33:46 +0330

    updated document service

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -28,6 +28,13 @@ class DocumentService
     {
         $fileName = Str::random() . '-' . $file->getClientOriginalName();
         $file->move(public_path() . $path, $fileName);
+        $destination = explode(',',$fileName);
+        if ($destination[1] == 'active_basij')
+        {
+            $doc->update([
+                'active_basij' => $fileName
+            ]);
+        }
         $doc->update([
             'war_history' => $fileName
         ]);
